6a2e01e2b78f7f52f051978d9568232a86a5567c
jinhyeok15 <ekfrhd97@gmail.com>
2022-09-01 21:39:30 +0900

    initial

---
+++ src/main/java/com/toss/tossBank/TossBankApplication.java
@@ -0,0 +1,13 @@
+package com.toss.tossBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TossBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toss/tossBank/TossBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toss.tossBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
