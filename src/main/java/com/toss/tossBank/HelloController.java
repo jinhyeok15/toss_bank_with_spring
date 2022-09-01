04168eb667f2bd71e55b67c315aa268e4d50ab15
jinhyeok15 <ekfrhd97@gmail.com>
2022-09-01 23:08:18 +0900

    feat: Spring tutorial 1

---
+++ src/main/java/com/toss/tossBank/HelloController.java
@@ -0,0 +1,12 @@
+package com.toss.tossBank;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
